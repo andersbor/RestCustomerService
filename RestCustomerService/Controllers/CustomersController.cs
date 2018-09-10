ddfaa94c3c529511b8d40bd508203ebaa61d9c1a
andersbor <anbo@easj.dk>
2018-09-10 11:53:57 +0200

    initial

---
+++ RestCustomerService/Controllers/CustomersController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RestCustomerService.model;
+
+namespace RestCustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private static readonly List<Customer> CList = new List<Customer>
+        {
+            new Customer {Id = 1, FirstName = "Anders", LastName = "B", Year = 2000}
+        };
+
+        // GET: api/Customers
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return CList;
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public Customer Get(int id)
+        {
+            return CList.FirstOrDefault(customer => customer.Id == id);
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public Customer Post([FromBody] Customer value)
+        {
+            value.Id = Customer.NextId++; ;
+            CList.Add(value);
+            return value;
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public Customer Put(int id, [FromBody] Customer value)
+        {
+            Customer c = CList.FirstOrDefault(customer => customer.Id == id);
+            if (c == null) return null;
+            c.FirstName = value.FirstName;
+            c.LastName = value.LastName;
+            c.Year = value.Year;
+            return c;
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public Customer Delete(int id)
+        {
+            Customer c = CList.FirstOrDefault(customer => customer.Id == id);
+            if (c == null) return null;
+            CList.Remove(c);
+            return c;
+        }
+    }
+}
---
+++ RestCustomerService/model/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace RestCustomerService.model
+{
+    public class Customer
+    {
+        public static int NextId = 2;
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+
+        /*public Customer()
+        {
+            
+        }*/
+
+        /*public Customer(int id, string first, string last, int year)
+        {
+
+        }*/
+    }
+}
