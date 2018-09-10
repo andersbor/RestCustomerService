eb3f7126247d090384ae2d79eb0f665b85c36c4f
andersbor <anbo@easj.dk>
2018-09-10 12:44:50 +0200

    CORS not working

--- RestCustomerService/Startup.cs
+++ RestCustomerService/Startup.cs
@@ -26,6 +26,7 @@ namespace RestCustomerService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,12 @@ namespace RestCustomerService
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCors(
+                options =>
+                {
+                    options.AllowAnyOrigin().AllowAnyMethod(); // allow everything from anywhere
+                });
+
         }
     }
 }
